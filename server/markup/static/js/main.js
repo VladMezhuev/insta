a7626b7a60188660e3dd5ef68b239c281a16099d
vlad <vlad.mezhuev11@gmail.com>
2021-09-15 10:36:07 -0400

    Init

---
+++ server/markup/static/js/main.js
@@ -0,0 +1,28 @@
+webpackJsonp([0],{
+
+/***/ 8:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(9);
+
+
+/***/ }),
+
+/***/ 9:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _interopRequireDefault = __webpack_require__(3);
+
+var _vue = _interopRequireDefault(__webpack_require__(1));
+
+window.app = new _vue["default"]({
+  el: '#app'
+});
+
+/***/ })
+
+},[8]);
+//# sourceMappingURL=main.js.map
